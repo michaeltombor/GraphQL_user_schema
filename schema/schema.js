452c436ae1d73d4e8d708681c1952f71f22e25e2
Michael Tombor <tombormt@gmail.com>
2018-04-06 16:39:11 +0000

    add resolve function for addUser mutation

--- schema/schema.js
+++ schema/schema.js
@@ -6,7 +6,8 @@ const {
     GraphQLString,
     GraphQLInt,
     GraphQLSchema,
-    GraphQLList
+    GraphQLList,
+    GraphQLNonNull
 } = graphql;
 
 const CompanyType = new GraphQLObjectType({
@@ -64,6 +65,26 @@ const RootQuery = new GraphQLObjectType ({
   }
 });
 
+const mutation = new GraphQLObjectType({
+  name: 'Mutation', 
+  fields: {
+    addUser: {
+      type: UserType,
+      args: { 
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
+        companyId: { type: GraphQLString }
+        
+      },
+      resolve(parentValue, { firstName, age }) {
+        return axios.post('https://modern-react-course-michaeltombor.c9users.io:8081/users', { firstName, age })
+        .then(res => res.data);
+      }
+    }
+  }
+});
+
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation
 });
